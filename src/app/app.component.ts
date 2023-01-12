6a7bcc2c771880aca50d878fd9f773c1efb1845b
Lizvan27 <lizvanmandy@gmail.com>
2023-01-12 13:57:13 -0500

    agregando productoJson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import ProductosJson from './productos.json';
+
+interface PRODUCTOS {
+  id: Number,
+  descripcion: String;
+  precio_compra: Number;
+  precio_venta: Number;
+  ganancia: Number;
+  existencia: Number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asincrona-17';
+
+  Productos : PRODUCTOS[] = ProductosJson;
+
+  constructor(){
+    console.log(this.Productos)
+  }
 }
